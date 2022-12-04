e6da6e70347840eb6912a99f1df6772527372261
Stefan Boos <kontakt@boos.systems>
2022-12-04 16:34:58 +0100

    feat: sum calories carried by single elf

--- AdventOfCode202201.Tests/ParserTests.cs
+++ AdventOfCode202201.Tests/ParserTests.cs
@@ -12,4 +12,17 @@ public class ParserTests
         
         Assert.Equal(expected, actual);
     }
+
+    [Fact]
+    public void TwoFoodItems()
+    {
+        const int firstItem = 200;
+        const int secondItem = 400;
+        const int expected = firstItem + secondItem;
+        string[] input = { firstItem.ToString(), secondItem.ToString() };
+        
+        var actual = Parser.Parse(input);
+        
+        Assert.Equal(expected, actual);
+    }
 }
--- AdventOfCode202201/Parser.cs
+++ AdventOfCode202201/Parser.cs
@@ -2,8 +2,8 @@ namespace AdventOfCode202201;
 
 public static class Parser
 {
-    public static int Parse(string[] _)
+    public static int Parse(string[] input)
     {
-        return 1000;
+        return input.Select(int.Parse).Sum();
     }
 }
