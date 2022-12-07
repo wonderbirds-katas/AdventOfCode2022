7954bdbb18acaa51be9daee23de9ff0253b922b3
Stefan Boos <kontakt@boos.systems>
2022-12-07 05:42:56 +0100

    test: use test's own representation of score

--- Day02RockPaperScissors/Day02RockPaperScissors.Tests/TurnTests.cs
+++ Day02RockPaperScissors/Day02RockPaperScissors.Tests/TurnTests.cs
@@ -1,16 +1,14 @@
-using static Day02RockPaperScissors.OutcomeScore;
-
 namespace Day02RockPaperScissors.Tests;
 
 public class TurnTests
 {
     [Theory]
-    [InlineData(Shape.Rock, Shape.Rock, ShapeScore.Rock, Draw, (int)ShapeScore.Rock + (int)OutcomeScore.Draw)]
-    [InlineData(Shape.Rock, Shape.Paper, ShapeScore.Paper, Win, (int)ShapeScore.Paper + (int)OutcomeScore.Win)]
-    public void OpponentVsOwnShapes(Shape opponent, Shape own, ShapeScore shapeScore, Day02RockPaperScissors.OutcomeScore outcomeScore, int sumScore)
+    [InlineData(Shape.Rock, Shape.Rock, ShapeScore.Rock, OutcomeScore.Draw, (int)ShapeScore.Rock + (int)OutcomeScore.Draw)]
+    [InlineData(Shape.Rock, Shape.Paper, ShapeScore.Paper, OutcomeScore.Win, (int)ShapeScore.Paper + (int)OutcomeScore.Win)]
+    public void OpponentVsOwnShapes(Shape opponent, Shape own, ShapeScore shapeScore, OutcomeScore outcomeScore, int sumScore)
     {
         var actual = Turn.Score(opponent, own);
-        var expected = new Score((int) shapeScore, outcomeScore);
+        var expected = new Score((int) shapeScore, (Day02RockPaperScissors.OutcomeScore)outcomeScore);
         Assert.Equal(expected, actual);
         Assert.Equal(sumScore, actual.Sum);
     }
@@ -21,7 +19,7 @@ public class TurnTests
         Paper = 2,
     }
 
-    private enum OutcomeScore
+    public enum OutcomeScore
     {
         Draw = 3,
         Win = 6,
