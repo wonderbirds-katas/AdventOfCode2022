4b0acb3d8335c260778c802a6b504fb071285272
Stefan Boos <kontakt@boos.systems>
2022-12-07 06:00:37 +0100

    refactor: extract method CalculateOutcome

--- Day02RockPaperScissors/Day02RockPaperScissors/Turn.cs
+++ Day02RockPaperScissors/Day02RockPaperScissors/Turn.cs
@@ -4,16 +4,23 @@ public static class Turn
 {
     public static Score Score(ShapeScore opponent, ShapeScore own)
     {
-        var outcome = OutcomeScore.Lose;
-        if (own == opponent)
-        {
-            outcome = OutcomeScore.Draw;
-        }
-        if (own > opponent)
-        {
-            outcome = OutcomeScore.Win;
-        }
+        var outcome = CalculateOutcome(opponent, own);
 
         return new Score(own, outcome);
+    }
+
+    private static OutcomeScore CalculateOutcome(ShapeScore opponent, ShapeScore own)
+    {
+        if (own == opponent)
+        {
+            return OutcomeScore.Draw;
+        }
+
+        if (own > opponent)
+        {
+            return OutcomeScore.Win;
+        }
+
+        return OutcomeScore.Lose;
     }
 }
