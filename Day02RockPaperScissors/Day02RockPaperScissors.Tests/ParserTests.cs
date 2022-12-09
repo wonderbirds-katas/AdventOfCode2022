9de9ac40c8db4a6dfc0a9559d4e5b4b4aa37f606
Stefan Boos <kontakt@boos.systems>
2022-12-09 06:04:28 +0100

    feat: parse single line (Rocks only)

---
+++ Day02RockPaperScissors/Day02RockPaperScissors.Tests/ParserTests.cs
@@ -0,0 +1,13 @@
+﻿namespace Day02RockPaperScissors.Tests;
+
+public class ParserTests
+{
+    [Fact]
+    public void Test()
+    {
+        (var opponentShape, var ownShape) = Parser.Parse("A X");
+
+        Assert.Equal(ShapeScore.Rock, opponentShape);
+        Assert.Equal(ShapeScore.Rock, ownShape);
+    }
+}
---
+++ Day02RockPaperScissors/Day02RockPaperScissors/Parser.cs
@@ -0,0 +1,9 @@
+﻿namespace Day02RockPaperScissors;
+
+public static class Parser
+{
+    public static (ShapeScore opponentShape, ShapeScore ownShape) Parse(string line)
+    {
+        return (ShapeScore.Rock, ShapeScore.Rock);
+    }
+}
