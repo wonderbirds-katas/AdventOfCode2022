1f4ff79254981fe0a0ef7ceab35b7497a714b47f
Stefan Boos <kontakt@boos.systems>
2022-12-09 17:01:14 +0100

    feat: parse "B X" to Paper and Rock

--- Day02RockPaperScissors/Day02RockPaperScissors.Tests/ParserParseLineTests.cs
+++ Day02RockPaperScissors/Day02RockPaperScissors.Tests/ParserParseLineTests.cs
@@ -4,7 +4,8 @@ public class ParserParseLineTests
 {
     [Theory]
     [InlineData("A X", ShapeScore.Rock, ShapeScore.Rock)]
-    public void RockVsRock(string line, ShapeScore expectedOpponentShape, ShapeScore expectedOwnShape)
+    [InlineData("B X", ShapeScore.Paper, ShapeScore.Rock)]
+    public void Test(string line, ShapeScore expectedOpponentShape, ShapeScore expectedOwnShape)
     {
         (var opponentShape, var ownShape) = Parser.ParseLine(line);
 
--- Day02RockPaperScissors/Day02RockPaperScissors/Parser.cs
+++ Day02RockPaperScissors/Day02RockPaperScissors/Parser.cs
@@ -4,6 +4,10 @@ public static class Parser
 {
     public static (ShapeScore opponentShape, ShapeScore ownShape) ParseLine(string line)
     {
+        if (line[0] == 'B')
+        {
+            return (ShapeScore.Paper, ShapeScore.Rock);
+        }
         return (ShapeScore.Rock, ShapeScore.Rock);
     }
 }
