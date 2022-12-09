4e7a2cd0b07326d7c86e6ce7d92c66ca0fab0a41
Stefan Boos <kontakt@boos.systems>
2022-12-09 17:36:57 +0100

    feat: parse the array of file contents

---
+++ Day02RockPaperScissors/Day02RockPaperScissors.Tests/FileContentsParserTests.cs
@@ -0,0 +1,11 @@
+﻿namespace Day02RockPaperScissors.Tests;
+
+public class FileContentsParserTests
+{
+    [Fact]
+    public void EmptyFileContents()
+    {
+        var actual = FileContentsParser.Parse(new string[] {});
+        Assert.Empty(actual);
+    }
+}
---
+++ Day02RockPaperScissors/Day02RockPaperScissors/FileContentsParser.cs
@@ -0,0 +1,9 @@
+﻿namespace Day02RockPaperScissors;
+
+public static class FileContentsParser
+{
+    public static IEnumerable<(ShapeScore, ShapeScore)> Parse(IEnumerable<string> fileLines)
+    {
+        yield break;
+    }
+}
