6f0d01498e605214bf2296990574069dcfdc5b20
Stefan Boos <kontakt@boos.systems>
2022-12-10 20:27:46 +0100

    feat: Program.Main assumes the strategy contains desired outcomes

--- Day02RockPaperScissors/Day02RockPaperScissors/Program.cs
+++ Day02RockPaperScissors/Day02RockPaperScissors/Program.cs
@@ -5,7 +5,7 @@ public static class Program
     public static void Main(string[] args)
     {
         var sum = File.ReadAllLines(args[0])
-            .Sum(line => line.Parse().Score().Sum);
+            .Sum(line => line.ParsePart2().CalculateRound().Score().Sum);
         Console.WriteLine(sum);
     }
 }
