09e52cba8a2bfe7d0670bda63de2bb83b59ce357
Stefan Boos <kontakt@boos.systems>
2022-12-10 19:56:24 +0100

    feat: find shape to win against paper

--- Day02RockPaperScissors/Day02RockPaperScissors.Tests/AdviceTests.cs
+++ Day02RockPaperScissors/Day02RockPaperScissors.Tests/AdviceTests.cs
@@ -5,6 +5,7 @@ public class AdviceTests
     [Theory]
     [InlineData(ShapeScore.Scissors, OutcomeScore.Win, ShapeScore.Rock)]
     [InlineData(ShapeScore.Rock, OutcomeScore.Win, ShapeScore.Paper)]
+    [InlineData(ShapeScore.Paper, OutcomeScore.Win, ShapeScore.Scissors)]
     public void CalculateRound(ShapeScore opponent, OutcomeScore desiredOutcome, ShapeScore expected)
     {
         var actual = new Advice(opponent, desiredOutcome).CalculateRound();
