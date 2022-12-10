96ec3133cbf6642cc421420da40c340b16c6844d
Stefan Boos <kontakt@boos.systems>
2022-12-10 20:31:59 +0100

    refactor: delete unused LineParser.Parse related code

--- Day02RockPaperScissors/Day02RockPaperScissors.Tests/LineParserTests.cs
+++ Day02RockPaperScissors/Day02RockPaperScissors.Tests/LineParserTests.cs
@@ -23,28 +23,6 @@ public class LineParserTests
         Assert.Equal(expectedOpponentShape, actual.Opponent);
     }
 
-    [Theory]
-    [InlineData("A X", ShapeScore.Rock)]
-    [InlineData("B X", ShapeScore.Paper)]
-    [InlineData("C X", ShapeScore.Scissors)]
-    public void OpponentShape(string line, ShapeScore expectedOpponentShape)
-    {
-        var actual = line.Parse();
-
-        Assert.Equal(expectedOpponentShape, actual.Opponent);
-    }
-
-    [Theory]
-    [InlineData("A X", ShapeScore.Rock)]
-    [InlineData("A Y", ShapeScore.Paper)]
-    [InlineData("A Z", ShapeScore.Scissors)]
-    public void OwnShape(string line, ShapeScore expectedOwnShape)
-    {
-        var actual = line.Parse();
-
-        Assert.Equal(expectedOwnShape, actual.Own);
-    }
-
     [Fact]
     public void InvalidOpponentShapeLiteral()
     {
--- Day02RockPaperScissors/Day02RockPaperScissors/LineParser.cs
+++ Day02RockPaperScissors/Day02RockPaperScissors/LineParser.cs
@@ -23,14 +23,6 @@ public static class LineParserExtensions
         return desiredOutcome;
     }
 
-    public static Round Parse(this string line)
-    {
-        var opponentShape = ParseOpponentShape(line);
-        var ownShape = ParseOwnShape(line);
-
-        return new Round(opponentShape, ownShape);
-    }
-
     private static ShapeScore ParseOpponentShape(string line)
     {
         var opponentShapeLiteral = line[0];
@@ -40,18 +32,6 @@ public static class LineParserExtensions
             'B' => ShapeScore.Paper,
             'C' => ShapeScore.Scissors,
             _ => throw new InvalidShapeLiteralException(opponentShapeLiteral)
-        };
-    }
-
-    private static ShapeScore ParseOwnShape(string line)
-    {
-        var ownShapeLiteral = line[2];
-        return ownShapeLiteral switch
-        {
-            'X' => ShapeScore.Rock,
-            'Y' => ShapeScore.Paper,
-            'Z' => ShapeScore.Scissors,
-            _ => throw new InvalidShapeLiteralException(ownShapeLiteral)
         };
     }
 }
