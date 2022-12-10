b10817b6e8c275056957b49e55b10e22748c004a
Stefan Boos <kontakt@boos.systems>
2022-12-10 18:56:20 +0100

    refactor: delegate Round.Score to RoundReplacement.Score

--- Day02RockPaperScissors/Day02RockPaperScissors/Round.cs
+++ Day02RockPaperScissors/Day02RockPaperScissors/Round.cs
@@ -1,18 +1,35 @@
 namespace Day02RockPaperScissors;
 
-public static class Round
+public class RoundReplacement
+{
+    private readonly ShapeScore _opponent;
+    private readonly ShapeScore _own;
+    
+    public RoundReplacement(ShapeScore opponent, ShapeScore own)
+    {
+        _opponent = opponent;
+        _own = own;
+    }
+    
+    public Score Score()
+    {
+        var outcome = CalculateOutcome();
+
+        return new Score(_own, outcome);
+    }
+
+    private OutcomeScore CalculateOutcome()
+    {
+        if (_own.IsGreaterThan(_opponent)) return OutcomeScore.Win;
+        if (_own == _opponent) return OutcomeScore.Draw;
+        return OutcomeScore.Lose;
+    }
+
+}
+public class Round
 {
     public static Score Score(ShapeScore opponent, ShapeScore own)
     {
-        var outcome = CalculateOutcome(opponent, own);
-
-        return new Score(own, outcome);
-    }
-
-    private static OutcomeScore CalculateOutcome(ShapeScore opponent, ShapeScore own)
-    {
-        if (own.IsGreaterThan(opponent)) return OutcomeScore.Win;
-        if (own == opponent) return OutcomeScore.Draw;
-        return OutcomeScore.Lose;
+        return new RoundReplacement(opponent, own).Score();
     }
 }
