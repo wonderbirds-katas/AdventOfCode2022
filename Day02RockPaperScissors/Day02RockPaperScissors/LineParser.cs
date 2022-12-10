6b592e3b71de7f6aca09d1d1c99745cecaf8d002
Stefan Boos <kontakt@boos.systems>
2022-12-10 19:24:44 +0100

    refactor: introduce layer of abstraction into LineParser.ParseShapes

--- Day02RockPaperScissors/Day02RockPaperScissors/LineParser.cs
+++ Day02RockPaperScissors/Day02RockPaperScissors/LineParser.cs
@@ -4,24 +4,33 @@ public static class LineParserExtensions
 {
     public static Round ParseShapes(this string line)
     {
+        var opponentShape = ParseOpponentShape(line);
+        var ownShape = ParseOwnShape(line);
+
+        return new Round(opponentShape, ownShape);
+    }
+
+    private static ShapeScore ParseOpponentShape(string line)
+    {
         var opponentShapeLiteral = line[0];
-        var opponentShape = opponentShapeLiteral switch
+        return opponentShapeLiteral switch
         {
             'A' => ShapeScore.Rock,
             'B' => ShapeScore.Paper,
             'C' => ShapeScore.Scissors,
             _ => throw new InvalidShapeLiteralException(opponentShapeLiteral)
         };
+    }
 
+    private static ShapeScore ParseOwnShape(string line)
+    {
         var ownShapeLiteral = line[2];
-        var ownShape = ownShapeLiteral switch
+        return ownShapeLiteral switch
         {
             'X' => ShapeScore.Rock,
             'Y' => ShapeScore.Paper,
             'Z' => ShapeScore.Scissors,
             _ => throw new InvalidShapeLiteralException(ownShapeLiteral)
         };
-
-        return new Round(opponentShape, ownShape);
     }
 }
