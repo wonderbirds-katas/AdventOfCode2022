6c88804ac87afded62af4dd5acb5bc5c8e40e6d6
Stefan Boos <kontakt@boos.systems>
2022-12-11 08:11:00 +0100

    tests: align RoundTests with RoundTests from day 2, part 1

--- Day02RockPaperScissors/Day02RockPaperScissors.Tests/RoundTests.cs
+++ Day02RockPaperScissors/Day02RockPaperScissors.Tests/RoundTests.cs
@@ -3,21 +3,22 @@ namespace Day02RockPaperScissors.Tests;
 public class RoundTests
 {
     [Theory]
-    [InlineData(ShapeScore.Scissors, OutcomeScore.Win, ShapeScore.Rock)]
-    [InlineData(ShapeScore.Rock, OutcomeScore.Win, ShapeScore.Paper)]
-    [InlineData(ShapeScore.Paper, OutcomeScore.Win, ShapeScore.Scissors)]
+    [InlineData(ShapeScore.Scissors, OutcomeScore.Win, ShapeScore.Rock, 1 + 6)]
+    [InlineData(ShapeScore.Rock, OutcomeScore.Win, ShapeScore.Paper, 2 + 6)]
+    [InlineData(ShapeScore.Paper, OutcomeScore.Win, ShapeScore.Scissors, 3 + 6)]
     
-    [InlineData(ShapeScore.Scissors, OutcomeScore.Draw, ShapeScore.Scissors)]
-    [InlineData(ShapeScore.Rock, OutcomeScore.Draw, ShapeScore.Rock)]
-    [InlineData(ShapeScore.Paper, OutcomeScore.Draw, ShapeScore.Paper)]
+    [InlineData(ShapeScore.Scissors, OutcomeScore.Draw, ShapeScore.Scissors, 3 + 3)]
+    [InlineData(ShapeScore.Rock, OutcomeScore.Draw, ShapeScore.Rock, 1 + 3)]
+    [InlineData(ShapeScore.Paper, OutcomeScore.Draw, ShapeScore.Paper, 2 + 3)]
     
-    [InlineData(ShapeScore.Scissors, OutcomeScore.Lose, ShapeScore.Paper)]
-    [InlineData(ShapeScore.Rock, OutcomeScore.Lose, ShapeScore.Scissors)]
-    [InlineData(ShapeScore.Paper, OutcomeScore.Lose, ShapeScore.Rock)]
-    public void Score(ShapeScore opponent, OutcomeScore desiredOutcome, ShapeScore expectedOwn)
+    [InlineData(ShapeScore.Scissors, OutcomeScore.Lose, ShapeScore.Paper, 2 + 0)]
+    [InlineData(ShapeScore.Rock, OutcomeScore.Lose, ShapeScore.Scissors, 3 + 0)]
+    [InlineData(ShapeScore.Paper, OutcomeScore.Lose, ShapeScore.Rock, 1 + 0)]
+    public void Score(ShapeScore opponent, OutcomeScore desiredOutcome, ShapeScore expectedOwn, int sumScore)
     {
         var actual = new Round(opponent, desiredOutcome).Score();
-        Assert.Equal(expectedOwn, actual.Shape);
-        Assert.Equal(desiredOutcome, actual.Outcome);
+        var expected = new Score(expectedOwn, desiredOutcome);
+        Assert.Equal(expected, actual);
+        Assert.Equal(sumScore, actual.Sum);
     }
 }
