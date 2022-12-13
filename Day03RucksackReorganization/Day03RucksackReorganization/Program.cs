b64e2f9414b91499bd62cc0d6d96415f46a1de6b
Stefan Boos <kontakt@boos.systems>
2022-12-13 07:51:27 +0100

    feat(infrastructure): two rucksacks with duplicates a a

--- Day03RucksackReorganization/Day03RucksackReorganization/Program.cs
+++ Day03RucksackReorganization/Day03RucksackReorganization/Program.cs
@@ -3,7 +3,28 @@ namespace Day03RucksackReorganization;
 public static class Program
 {
     public static void Main(string[] args)
-    { 
-        Console.WriteLine("1");
+    {
+        var sum = File.ReadAllLines(args[0])
+            .Sum(line => line.Parse().Analyze().Priority);
+        Console.WriteLine($"{sum}");
+    }
+
+    private static Item Analyze(this Rucksack rucksack)
+    {
+        return new Item();
+    }
+
+    private static Rucksack Parse(this string line)
+    {
+        return new Rucksack();
+    }
+
+    private class Rucksack
+    {
+    }
+
+    private class Item
+    {
+        public int Priority => 1;
     }
 }
