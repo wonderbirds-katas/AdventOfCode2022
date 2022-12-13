c2e6d0d7bbfa5a8c87700209aca971d4d06625b6
Stefan Boos <kontakt@boos.systems>
2022-12-13 07:42:14 +0100

    test: two rucksacks with duplicates a a

--- Day03RucksackReorganization/Day03RucksackReorganization.Tests/ProgramTests.cs
+++ Day03RucksackReorganization/Day03RucksackReorganization.Tests/ProgramTests.cs
@@ -4,6 +4,7 @@ public class ProgramTests
 {
     [Theory]
     [InlineData("./data/single_rucksack_with_duplicate_a.txt", 1)]
+    [InlineData("./data/two_rucksacks_with_duplicates_a_a.txt", 2)]
     public void ProcessInputFile(string testFileName, int expected)
     {
         var stringWriter = new StringWriter();
