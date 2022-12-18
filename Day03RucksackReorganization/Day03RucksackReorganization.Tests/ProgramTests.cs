cb08c507019b17b0cb607fbf78ef7dd71a1a9734
Stefan Boos <kontakt@boos.systems>
2022-12-18 13:43:07 +0100

    test(Program): add test data from advent of code puzzle description

--- Day03RucksackReorganization/Day03RucksackReorganization.Tests/ProgramTests.cs
+++ Day03RucksackReorganization/Day03RucksackReorganization.Tests/ProgramTests.cs
@@ -6,6 +6,7 @@ public class ProgramTests
     [InlineData("./data/single_rucksack_with_duplicate_a.txt", 1)]
     [InlineData("./data/two_rucksacks_with_duplicates_a_a.txt", 2)]
     [InlineData("./data/two_rucksacks_with_duplicates_b_b.txt", 4)]
+    [InlineData("./data/advent_of_code_example.txt", 157)]
     public void ProcessInputFile(string testFileName, int expected)
     {
         var stringWriter = new StringWriter();
