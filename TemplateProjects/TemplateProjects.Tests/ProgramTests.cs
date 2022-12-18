9b8e51f18c66fbae7071d32cb67c4c9ba67f450a
Stefan Boos <kontakt@boos.systems>
2022-12-18 13:57:46 +0100

    style: run dotnet csharpier . in template project

--- TemplateProjects/TemplateProjects.Tests/ProgramTests.cs
+++ TemplateProjects/TemplateProjects.Tests/ProgramTests.cs
@@ -8,9 +8,9 @@ public class ProgramTests
     {
         var stringWriter = new StringWriter();
         Console.SetOut(stringWriter);
-        
-        Program.Main(new[] {testFileName});
-        
+
+        Program.Main(new[] { testFileName });
+
         Assert.Equal($"Input file is '{testFileName}'\n", stringWriter.ToString());
     }
 }
--- TemplateProjects/TemplateProjects/Program.cs
+++ TemplateProjects/TemplateProjects/Program.cs
@@ -3,7 +3,7 @@ namespace TemplateProjects;
 public static class Program
 {
     public static void Main(string[] args)
-    { 
+    {
         Console.WriteLine($"Input file is '{args[0]}'");
     }
 }
