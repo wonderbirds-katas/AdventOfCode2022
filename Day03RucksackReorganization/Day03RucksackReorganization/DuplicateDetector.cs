6299f850c4075f0b1141ae9b04cb5246d832ff1f
Stefan Boos <kontakt@boos.systems>
2022-12-19 05:14:22 +0100

    refactor: remove obsolete DuplicateDetector

--- Day03RucksackReorganization/Day03RucksackReorganization/DuplicateDetector.cs
+++
@@ -1,9 +0,0 @@
-namespace Day03RucksackReorganization;
-
-public static class DuplicateDetector
-{
-    public static Item Analyze(this Rucksack rucksack)
-    {
-        return rucksack.FirstCompartment.Items.Intersect(rucksack.SecondCompartment.Items).First();
-    }
-}
