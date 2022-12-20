4b8ca314c360a58f07805fe246fdf1728f10dbff
Stefan Boos <kontakt@boos.systems>
2022-12-20 22:00:12 +0100

    refactor(tests): inline variable

--- Day04CampCleanup/Day04CampCleanup.Tests/ElfPairParserTests.cs
+++ Day04CampCleanup/Day04CampCleanup.Tests/ElfPairParserTests.cs
@@ -5,9 +5,9 @@ public class ElfPairParserTests
     [Theory]
     [InlineData("1-1,2-2", new []{ 1 }, new[] { 2 })]
     [InlineData("99-99,123-123", new []{ 99 }, new[] { 123 })]
-    public void NextTestWithoutMeaningfulName(string input, int[] expectedFirstElfSections, int[] expectedSecondElfSections)
+    public void RangeConsistsOfSingleNumber(string input, int[] expectedFirstElfSections, int[] expectedSecondElfSections)
     {
-        var actual = input.Parse();
-        actual.Should().ContainSections(expectedFirstElfSections, expectedSecondElfSections);
+        input.Parse()
+            .Should().ContainSections(expectedFirstElfSections, expectedSecondElfSections);
     }
 }
