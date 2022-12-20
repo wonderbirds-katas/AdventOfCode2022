da17bff16dfc3e2ddea7387ab59af2b14a28d367
Stefan Boos <kontakt@boos.systems>
2022-12-20 05:34:36 +0100

    test: add test for my personal data

--- Day03RucksackReorganization/Day03RucksackReorganization.Tests/ProgramTests.cs
+++ Day03RucksackReorganization/Day03RucksackReorganization.Tests/ProgramTests.cs
@@ -5,7 +5,7 @@ public class ProgramTests
     [Theory]
     [InlineData("./data/single_group_with_shared_a.txt", 1)]
     [InlineData("./data/advent_of_code_example.txt", 70)]
-    // [InlineData("./data/advent_of_code_stefans_personal_data.txt", 8243)]
+    [InlineData("./data/advent_of_code_stefans_personal_data.txt", 2631)]
     public void ProcessInputFile(string testFileName, int expected)
     {
         var stringWriter = new StringWriter();
