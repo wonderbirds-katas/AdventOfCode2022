2050a05e9a203fc09ba7b2df6dbb73d9e8e1c210
Stefan Boos <kontakt@boos.systems>
2022-12-20 06:02:44 +0100

    docs: design pipeline as comments in Main

--- Day04CampCleanup/Day04CampCleanup/Program.cs
+++ Day04CampCleanup/Day04CampCleanup/Program.cs
@@ -1,9 +1,16 @@
+using System.Collections;
+
 namespace Day04CampCleanup;
 
 public static class Program
 {
     public static void Main(string[] args)
     {
+        // TODO: Implement this pipeline
+        // var lines = File.ReadAllLines(args[0]);
+        // IEnumerable<ElfPair> elfPairs = lines.Parse();
+        // elfPairs.Where(IsOneSectionFullyContained).Count();
+
         Console.WriteLine($"Input file is '{args[0]}'");
     }
 }
