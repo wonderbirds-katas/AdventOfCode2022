9760e4d1f48ce3167a68f0268f8230afcf4a639e
Stefan Boos <kontakt@boos.systems>
2022-12-20 21:27:48 +0100

    feat: find fully contained intersections

--- Day04CampCleanup/Day04CampCleanup.Tests/ElfPairTests.cs
+++ Day04CampCleanup/Day04CampCleanup.Tests/ElfPairTests.cs
@@ -6,7 +6,20 @@ public class ElfPairTests
     [InlineData(new[] { 1 }, new[] { 2 })]
     public void NotFullyIntersectingSectionAssignments(int[] firstElfSections, int[] secondElfSections)
     {
-        var actual = new ElfPair(firstElfSections, secondElfSections).IsOneSectionFullyContained();
-        actual.Should().Be(false);
+        new ElfPair(firstElfSections, secondElfSections)
+            .IsOneSectionFullyContained()
+            .Should().Be(false);
+    }
+
+    [Theory]
+    [InlineData(new[] { 2 }, new[] { 2 })]
+    [InlineData(new[] { 10, 11, 12 }, new[] { 10, 11, 12 })]
+    [InlineData(new[] { 10, 11, 12 }, new[] { 9, 10, 11, 12, 13 })]
+    [InlineData(new[] { 9, 10, 11, 12, 13 }, new[] { 10, 11, 12 })]
+    public void FullyIntersectingSectionAssignments(int[] firstElfSections, int[] secondElfSections)
+    {
+        new ElfPair(firstElfSections, secondElfSections)
+            .IsOneSectionFullyContained()
+            .Should().Be(true);
     }
 }
--- Day04CampCleanup/Day04CampCleanup/ElfPair.cs
+++ Day04CampCleanup/Day04CampCleanup/ElfPair.cs
@@ -2,12 +2,21 @@ namespace Day04CampCleanup;
 
 public class ElfPair
 {
+    private readonly int[] _firstElfSections;
+    private readonly int[] _secondElfSections;
+
     public ElfPair(int[] firstElfSections, int[] secondElfSections)
     {
+        _firstElfSections = firstElfSections;
+        _secondElfSections = secondElfSections;
     }
 
     public bool IsOneSectionFullyContained()
     {
-        return false;
+        // TODO: Algorithm is easy to understand, but performance could be better!
+        var intersection = _firstElfSections.Intersect(_secondElfSections).ToList();
+        
+        return intersection.SequenceEqual(_firstElfSections) ||
+               intersection.SequenceEqual(_secondElfSections);
     }
 }
