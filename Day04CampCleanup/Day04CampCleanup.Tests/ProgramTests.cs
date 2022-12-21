4501624cd8f719d1c71c9f9e43e716898235a338
Stefan Boos <kontakt@boos.systems>
2022-12-21 12:05:45 +0100

    test: add advent of code data files

--- Day04CampCleanup/Day04CampCleanup.Tests/ProgramTests.cs
+++ Day04CampCleanup/Day04CampCleanup.Tests/ProgramTests.cs
@@ -4,6 +4,8 @@ public class ProgramTests
 {
     [Theory]
     [InlineData("./data/single_pair_no_overlap.txt", 0)]
+    [InlineData("./data/advent_of_code_example.txt", 2)]
+    [InlineData("./data/advent_of_code_stefans_personal_data.txt", 599)]
     public void ProcessInputFile(string testFileName, int expected)
     {
         var stringWriter = new StringWriter();
