92350843c8273adbe2f75bf98c9fcee5f7e0e686
Stefan Boos <kontakt@boos.systems>
2022-12-23 22:24:54 +0100

    test: improve test descriptions

--- Day05SupplyStacks/Day05SupplyStacks.Tests/Adapters/StockSpecificationDeserializerTests.cs
+++ Day05SupplyStacks/Day05SupplyStacks.Tests/Adapters/StockSpecificationDeserializerTests.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using Day05SupplyStacks.Adapters;
 using Day05SupplyStacks.Model;
 
@@ -7,48 +6,40 @@ namespace Day05SupplyStacks.Tests.Adapters;
 public class StockSpecificationDeserializerTests
 {
     [Theory]
-    [ClassData(typeof(StockSpecificationDeserializerTestData))]
-    public void Deserialize(string[] input, OrderedStock expected)
+    [MemberData(nameof(TestCase.TestCaseData), MemberType = typeof(TestCase))]
+    public void Deserialize2(TestCase testCase)
     {
-        var actual = StockSpecificationDeserializer.Deserialize(input);
-        actual.Should().BeEquivalentTo(expected);
+        var actual = StockSpecificationDeserializer.Deserialize(testCase.Input);
+        actual.Should().BeEquivalentTo(testCase.Expected);
     }
 
-    private class StockSpecificationDeserializerTestData : IEnumerable<object[]>
+    public record TestCase(string Description, string[] Input, OrderedStock Expected)
     {
-        public IEnumerator<object[]> GetEnumerator()
+        private static readonly TestCase[] TestCases =
         {
-            yield return new object[]
-            {
-                new[] { "1" },
+            new(
+                "1 stack, 0 crates",
+                new[] { " 1 " },
                 OrderedStockBuilder.WithNumberOfStacks(1).Build()
-            };
-
-            yield return new object[]
-            {
-                new[] {
-                    "[A]",
-                    " 1 "
-                },
-                OrderedStockBuilder.WithNumberOfStacks(1)
-                    .AddCratesToStack(new []{ 'A' }, 0)
+            ),
+            new(
+                "1 stack, 1 crate",
+                new[] { "[A]", " 1 " },
+                OrderedStockBuilder.WithNumberOfStacks(1).AddCratesToStack(new[] { 'A' }, 0).Build()
+            ),
+            new(
+                "1 stack, 3 crates",
+                new[] { "[C]", "[B]", "[A]", " 1 " },
+                OrderedStockBuilder
+                    .WithNumberOfStacks(1)
+                    .AddCratesToStack(new[] { 'A', 'B', 'C' }, 0)
                     .Build()
-            };
+            ),
+        };
 
-            yield return new object[]
-            {
-                new[] {
-                    "[C]",
-                    "[B]",
-                    "[A]",
-                    " 1 "
-                },
-                OrderedStockBuilder.WithNumberOfStacks(1)
-                    .AddCratesToStack(new []{ 'A', 'B', 'C' }, 0)
-                    .Build()
-            };
-        }
+        public static IEnumerable<object[]> TestCaseData =>
+            TestCases.Select(testCase => new object[] { testCase });
 
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        public override string ToString() => Description;
     }
 }
