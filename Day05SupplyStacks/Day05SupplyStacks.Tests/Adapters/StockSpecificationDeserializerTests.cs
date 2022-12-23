8947bc70ca14df654dde19a082be91b1d7a104f4
Stefan Boos <kontakt@boos.systems>
2022-12-23 15:29:39 +0100

    feat: deserialize single empty crate stack

--- Day05SupplyStacks/Day05SupplyStacks.Tests/Adapters/StockSpecificationDeserializerTests.cs
+++ Day05SupplyStacks/Day05SupplyStacks.Tests/Adapters/StockSpecificationDeserializerTests.cs
@@ -5,21 +5,21 @@ namespace Day05SupplyStacks.Tests.Adapters;
 
 public class StockSpecificationDeserializerTests
 {
-    private static readonly string[] SingleEmptyStack = { "1" };
+    private static readonly string[] SingleEmptyStackSerialized = { "1" };
 
     [Theory]
     [ClassData(typeof(StockSpecificationDeserializerTestData))]
     public void NextTestWithoutMeaningfulName(string[] input, OrderedStack expected)
     {
-        var actual = new OrderedStack();
-        actual.Should().BeOfType(typeof(OrderedStack));
+        var actual = new OrderedStack(new List<CrateStack> { new() });
+        actual.Should().BeEquivalentTo(expected);
     }
 
     private class StockSpecificationDeserializerTestData : IEnumerable<object []>
     {
         public IEnumerator<object[]> GetEnumerator()
         {
-            yield return new object[] {SingleEmptyStack, new OrderedStack()};
+            yield return new object[] {SingleEmptyStackSerialized, new OrderedStack(new List<CrateStack> { new() })};
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
--- Day05SupplyStacks/Day05SupplyStacks/Model/OrderedStack.cs
+++ Day05SupplyStacks/Day05SupplyStacks/Model/OrderedStack.cs
@@ -2,4 +2,10 @@ namespace Day05SupplyStacks.Model;
 
 public class OrderedStack
 {
+    public List<CrateStack> CrateStacks { get; }
+
+    public OrderedStack(List<CrateStack> crateStacks)
+    {
+        CrateStacks = crateStacks;
+    }
 }
