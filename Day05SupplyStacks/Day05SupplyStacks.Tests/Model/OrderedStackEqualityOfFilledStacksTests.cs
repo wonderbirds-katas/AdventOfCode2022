3584ebca8bdcf5a8ab03ebb9d69bc1004c4859f8
Stefan Boos <kontakt@boos.systems>
2022-12-23 22:24:22 +0100

    style: dotnet csharpier .

--- Day05SupplyStacks/Day05SupplyStacks.Tests/Model/OrderedStackEqualityOfFilledStacksTests.cs
+++ Day05SupplyStacks/Day05SupplyStacks.Tests/Model/OrderedStackEqualityOfFilledStacksTests.cs
@@ -7,28 +7,32 @@ public class OrderedStackEqualityOfFilledStacksTests
     [Fact]
     public void SameStacks()
     {
-        var left = OrderedStockBuilder.WithNumberOfStacks(1)
-            .AddCratesToStack(new []{ 'A' }, 0)
+        var left = OrderedStockBuilder
+            .WithNumberOfStacks(1)
+            .AddCratesToStack(new[] { 'A' }, 0)
             .Build();
 
-        var right = OrderedStockBuilder.WithNumberOfStacks(1)
-            .AddCratesToStack(new []{ 'A' }, 0)
+        var right = OrderedStockBuilder
+            .WithNumberOfStacks(1)
+            .AddCratesToStack(new[] { 'A' }, 0)
             .Build();
 
         left.Equals(right).Should().Be(true);
     }
 
     [Theory]
-    [InlineData(new[] {'A'}, new[] {'B'})]
-    [InlineData(new[] {'A','B','C'}, new[] {'B'})]
-    [InlineData(new[] {'A','B','C'}, new[] {'C','B','A'})]
+    [InlineData(new[] { 'A' }, new[] { 'B' })]
+    [InlineData(new[] { 'A', 'B', 'C' }, new[] { 'B' })]
+    [InlineData(new[] { 'A', 'B', 'C' }, new[] { 'C', 'B', 'A' })]
     public void DifferentStacks(char[] leftCrates, char[] rightCrates)
     {
-        var left = OrderedStockBuilder.WithNumberOfStacks(1)
+        var left = OrderedStockBuilder
+            .WithNumberOfStacks(1)
             .AddCratesToStack(leftCrates, 0)
             .Build();
 
-        var right = OrderedStockBuilder.WithNumberOfStacks(1)
+        var right = OrderedStockBuilder
+            .WithNumberOfStacks(1)
             .AddCratesToStack(rightCrates, 0)
             .Build();
 
@@ -38,18 +42,20 @@ public class OrderedStackEqualityOfFilledStacksTests
     [Fact]
     public void MultipleStacksWithEqualCrates()
     {
-        var left = OrderedStockBuilder.WithNumberOfStacks(3)
-            .AddCratesToStack(new []{'A', 'B'}, 0)
-            .AddCratesToStack(new []{'C', 'D', 'E'}, 1)
-            .AddCratesToStack(new []{'F', 'G', 'H', 'I'}, 2)
+        var left = OrderedStockBuilder
+            .WithNumberOfStacks(3)
+            .AddCratesToStack(new[] { 'A', 'B' }, 0)
+            .AddCratesToStack(new[] { 'C', 'D', 'E' }, 1)
+            .AddCratesToStack(new[] { 'F', 'G', 'H', 'I' }, 2)
             .Build();
 
-        var right = OrderedStockBuilder.WithNumberOfStacks(3)
-            .AddCratesToStack(new []{'A', 'B'}, 0)
-            .AddCratesToStack(new []{'C', 'D', 'E'}, 1)
-            .AddCratesToStack(new []{'F', 'G', 'H', 'I'}, 2)
+        var right = OrderedStockBuilder
+            .WithNumberOfStacks(3)
+            .AddCratesToStack(new[] { 'A', 'B' }, 0)
+            .AddCratesToStack(new[] { 'C', 'D', 'E' }, 1)
+            .AddCratesToStack(new[] { 'F', 'G', 'H', 'I' }, 2)
             .Build();
-        
+
         left.Should().Be(right);
     }
 
@@ -57,19 +63,21 @@ public class OrderedStackEqualityOfFilledStacksTests
     public void MultipleStacksWithDifferentCrates()
     {
         const char Difference = 'Z';
-        
-        var left = OrderedStockBuilder.WithNumberOfStacks(3)
-            .AddCratesToStack(new []{'A', 'B'}, 0)
-            .AddCratesToStack(new []{'C', 'D', 'E'}, 1)
-            .AddCratesToStack(new []{'F', 'G', 'H', 'I'}, 2)
+
+        var left = OrderedStockBuilder
+            .WithNumberOfStacks(3)
+            .AddCratesToStack(new[] { 'A', 'B' }, 0)
+            .AddCratesToStack(new[] { 'C', 'D', 'E' }, 1)
+            .AddCratesToStack(new[] { 'F', 'G', 'H', 'I' }, 2)
             .Build();
 
-        var right = OrderedStockBuilder.WithNumberOfStacks(3)
-            .AddCratesToStack(new []{'A', 'B'}, 0)
-            .AddCratesToStack(new []{'C', 'D', Difference}, 1)
-            .AddCratesToStack(new []{'F', 'G', 'H', 'I'}, 2)
+        var right = OrderedStockBuilder
+            .WithNumberOfStacks(3)
+            .AddCratesToStack(new[] { 'A', 'B' }, 0)
+            .AddCratesToStack(new[] { 'C', 'D', Difference }, 1)
+            .AddCratesToStack(new[] { 'F', 'G', 'H', 'I' }, 2)
             .Build();
-        
+
         left.Should().NotBe(right);
     }
 }
--- Day05SupplyStacks/Day05SupplyStacks/Model/OrderedStockBuilder.cs
+++ Day05SupplyStacks/Day05SupplyStacks/Model/OrderedStockBuilder.cs
@@ -8,13 +8,13 @@ public class OrderedStockBuilder
     {
         _stacks = Enumerable.Range(0, numberOfStacks).Select(_ => new CrateStack()).ToList();
     }
-    
+
     public OrderedStockBuilder AddCratesToStack(IEnumerable<char> crates, int stackIndex)
     {
         var target = _stacks[stackIndex];
-        
+
         crates.ToList().ForEach(crate => target.AddOnTop(crate));
-        
+
         return this;
     }
 
