90cd2d86af3553dff8b36337554308fb6d024be3
Stefan Boos <kontakt@boos.systems>
2022-12-23 19:24:12 +0100

    refactor: simplify StockSpecificationDeserializer

--- Day05SupplyStacks/Day05SupplyStacks/Adapters/StockSpecificationDeserializer.cs
+++ Day05SupplyStacks/Day05SupplyStacks/Adapters/StockSpecificationDeserializer.cs
@@ -7,16 +7,10 @@ public static class StockSpecificationDeserializer
     public static OrderedStock Deserialize(string[] input)
     {
         var builder = OrderedStockBuilder.WithNumberOfStacks(1);
-        
-        for (var row = input.Length - 1; row >= 0; row--)
-        {
-            if (input[row][0] == '[')
-            {
-                var crate = input[row][1];
-                builder.AddCratesToStack(new[] {crate}, 0);
-            }    
-        }
-        
+        var reversedStacksInput = input.Reverse().Skip(1).ToList();
+
+        reversedStacksInput.ForEach(line => builder.AddCratesToStack(new[] { line[1] }, 0));
+
         return builder.Build();
     }
 }
