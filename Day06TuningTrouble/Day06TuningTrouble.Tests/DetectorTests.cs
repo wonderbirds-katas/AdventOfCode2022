d03b38dc0d5cdd8c8a2456c69b219b9098f74a0b
Stefan Boos <kontakt@boos.systems>
2022-12-27 14:33:28 +0100

    feat: one character before start of packet marker

--- Day06TuningTrouble/Day06TuningTrouble.Tests/DetectorTests.cs
+++ Day06TuningTrouble/Day06TuningTrouble.Tests/DetectorTests.cs
@@ -7,12 +7,23 @@ public class DetectorTests
     {
         Detector.CountCharactersBeforeStartOfPacketMarker("abcd").Should().Be(4);
     }
+
+    [Fact]
+    public void OneCharacterBeforeStartOfPacketMarker()
+    {
+        Detector.CountCharactersBeforeStartOfPacketMarker("aabcd").Should().Be(5);
+    }
 }
 
 public static class Detector
 {
     public static int CountCharactersBeforeStartOfPacketMarker(string input)
     {
+        var buffer = input.Substring(0, 4).ToHashSet();
+        
+        if (buffer.Count != 4)
+            return 5;
+        
         return 4;
     }
 }
