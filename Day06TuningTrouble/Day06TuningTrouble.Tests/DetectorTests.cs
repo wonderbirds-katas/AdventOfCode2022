9b53fa01f21b7c2d9ff4075468e375a77336171e
Stefan Boos <kontakt@boos.systems>
2022-12-27 14:47:21 +0100

    feat: two characters before start of packet marker

--- Day06TuningTrouble/Day06TuningTrouble.Tests/DetectorTests.cs
+++ Day06TuningTrouble/Day06TuningTrouble.Tests/DetectorTests.cs
@@ -5,6 +5,7 @@ public class DetectorTests
     [Theory]
     [InlineData("abcd", 4)]
     [InlineData("aabcd", 5)]
+    [InlineData("aaabcd", 6)]
     public void RunTest(string input, int expected) => Detector.CountCharactersBeforeStartOfPacketMarker(input).Should().Be(expected);
 }
 
@@ -12,11 +13,15 @@ public static class Detector
 {
     public static int CountCharactersBeforeStartOfPacketMarker(string input)
     {
-        var buffer = input.Substring(0, 4).ToHashSet();
-        
-        if (buffer.Count != 4)
-            return 5;
-        
-        return 4;
+        var offset = -1;
+        bool detected;
+
+        do
+        {
+            ++offset;
+            detected = input.Substring(offset, 4).ToHashSet().Count == 4;
+        } while (!detected);
+
+        return 4 + offset;
     }
 }
