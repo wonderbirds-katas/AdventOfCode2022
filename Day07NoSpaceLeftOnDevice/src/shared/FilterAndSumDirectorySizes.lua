b1c72635b4829ceb05d032c89b8dc24e28f750a9
Stefan Boos <kontakt@boos.systems>
2022-12-28 20:16:21 +0100

    feat: root directory contains arbitrary number of files

--- Day07NoSpaceLeftOnDevice/src/shared/FilterAndSumDirectorySizes.lua
+++ Day07NoSpaceLeftOnDevice/src/shared/FilterAndSumDirectorySizes.lua
@@ -1,8 +1,18 @@
 local M = {}
 
 function M.FilterAndSumDirectorySizes(input)
-	_, _, size = string.find(input, "(%d+)")
-	return tonumber(size)
+	sum = 0
+	startIndex = 0
+	endIndex = -1
+
+	repeat
+		_, endIndex, size = string.find(input, "(%d+)", endIndex + 1)
+		if size ~= nil then
+			sum = sum + size
+		end
+	until size == nil
+
+	return tonumber(sum)
 end
 
 return M
--- Day07NoSpaceLeftOnDevice/test/FilterAndSumDirectorySizesTests.lua
+++ Day07NoSpaceLeftOnDevice/test/FilterAndSumDirectorySizesTests.lua
@@ -9,22 +9,40 @@ $ ls
 ]]
 end
 
-function test_root_directory_contains_single_file_with_size___1()
+function test_root_directory_contains_1_file__with_size___1()
     local input = cd_root_and_ls_returns("1 some_file")
     local actual = f.FilterAndSumDirectorySizes(input)
     lu.assertEquals(actual, 1)
 end
 
-function test_root_directory_contains_single_file_with_size___2()
+function test_root_directory_contains_1_file__with_size___2()
     local input = cd_root_and_ls_returns("2 some_file")
     local actual = f.FilterAndSumDirectorySizes(input)
     lu.assertEquals(actual, 2)
 end
 
-function test_root_directory_contains_single_file_with_size_100()
+function test_root_directory_contains_1_file__with_size_100()
     local input = cd_root_and_ls_returns("100 some_file")
     local actual = f.FilterAndSumDirectorySizes(input)
     lu.assertEquals(actual, 100)
+end
+
+function test_root_directory_contains_2_files_with_size_1_each()
+    local input = cd_root_and_ls_returns("1 file_a\n1 file_b")
+    local actual = f.FilterAndSumDirectorySizes(input)
+    lu.assertEquals(actual, 2)
+end
+
+function test_root_directory_contains_2_files_with_size_2_each()
+    local input = cd_root_and_ls_returns("2 file_a\n2 file_b")
+    local actual = f.FilterAndSumDirectorySizes(input)
+    lu.assertEquals(actual, 4)
+end
+
+function test_root_directory_contains_4_files_with_different_sizes()
+    local input = cd_root_and_ls_returns("200 file_a\n1000 file_b\n10000 file_c\n8800 file_d")
+    local actual = f.FilterAndSumDirectorySizes(input)
+    lu.assertEquals(actual, 20000)
 end
 
 os.exit(lu.LuaUnit.run())
