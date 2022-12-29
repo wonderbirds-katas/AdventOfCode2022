5bc2da607f6b84c090383fdbe46601b655e4f240
Stefan Boos <kontakt@boos.systems>
2022-12-29 20:53:06 +0100

    feat: parse two different files

--- Day07NoSpaceLeftOnDevice/src/shared/TerminalOutputParser.lua
+++ Day07NoSpaceLeftOnDevice/src/shared/TerminalOutputParser.lua
@@ -3,8 +3,23 @@ local fs = require('FileSystem')
 local M = {}
 
 function M.Parse(terminalOutput)
+
     local result = fs.CreateDirectory("/", nil)
-    fs.CreateFile("a", 1, result)
+
+    local filePattern = "(%d+%s%a+)"
+    local line
+    local endIndex = -1
+
+    repeat
+        local size, filename
+        _, endIndex, line = string.find(terminalOutput, filePattern, endIndex + 1)
+
+        if line ~= nil then
+            _, _, size, filename = string.find(line, "(%d+)%s(%a+)", 0)
+            fs.CreateFile(filename, size, result)
+        end
+    until line == nil
+
     return result
 end
 
--- Day07NoSpaceLeftOnDevice/test/TerminalOutputParserTests.lua
+++ Day07NoSpaceLeftOnDevice/test/TerminalOutputParserTests.lua
@@ -5,11 +5,34 @@ local fs = require('FileSystem')
 TestTerminalOutputParser = {}
 
 function TestTerminalOutputParser.test_root_directory_contains_1_file()
+    local terminalOutput = [[$ cd /
+$ ls
+1 a
+]]
+
     local root = parser.Parse(terminalOutput)
-    actual = fs.ToString(root)
+    local actual = fs.ToString(root)
+
     lu.assertEquals(actual,
 [[/ (dir)
 +-- a (file, size=1)
+]])
+end
+
+function TestTerminalOutputParser.test_root_directory_contains_2_files()
+    local terminalOutput = [[$ cd /
+$ ls
+1 a
+42 b
+]]
+
+    local root = parser.Parse(terminalOutput)
+    local actual = fs.ToString(root)
+
+    lu.assertEquals(actual,
+[[/ (dir)
++-- a (file, size=1)
++-- b (file, size=42)
 ]])
 end
 
