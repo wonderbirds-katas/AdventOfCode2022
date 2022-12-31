0089600027edbda72d3009640cff1bed741fe9d4
Stefan Boos <kontakt@boos.systems>
2022-12-31 19:12:27 +0100

    test: add example of advent of code

--- Day07NoSpaceLeftOnDevice/test/FilterAndSumDirectorySizesTests.lua
+++ Day07NoSpaceLeftOnDevice/test/FilterAndSumDirectorySizesTests.lua
@@ -107,4 +107,34 @@ $ ls
     lu.assertEquals(actual, 100)
 end
 
+function TestFilterAndSumDirectorySizes:test_advent_of_code_example()
+    local input =
+[[$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+]]
+    local actual = f.FilterAndSumDirectorySizes(input)
+    lu.assertEquals(actual, 95437)
+end
+
 return TestFilterAndSumDirectorySizes
