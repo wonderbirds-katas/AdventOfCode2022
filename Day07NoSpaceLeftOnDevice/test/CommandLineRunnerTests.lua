2a82e9293f3f0e509281031ab502e766d2c6a578
Stefan Boos <kontakt@boos.systems>
2023-01-03 06:22:43 +0100

    refactor(tests): extract class PrintArgumentInterceptor

--- Day07NoSpaceLeftOnDevice/test/CommandLineRunnerTests.lua
+++ Day07NoSpaceLeftOnDevice/test/CommandLineRunnerTests.lua
@@ -1,64 +1,15 @@
 local lu = require('luaunit')
 local clr = require('CommandLineRunner')
+local pai = require('PrintArgumentInterceptor')
 
 TestCommandLineRunner = {}
 
-local savedOutput = {}
-local function SaveOutput(...)
-    table.insert(savedOutput, {...})
-end
-
-local originalPrint = print
-local function InterceptedPrint(...)
-    SaveOutput(...)
-    -- Uncomment the following line if you want to see the output for debugging
-    -- originalPrint(...)
-end
-
-local function ConvertSavedOutputToString()
-    local result = ""
-    for _, printArgs in ipairs(savedOutput) do
-        local printArgsAsString = ""
-        for _, printArg in ipairs(printArgs) do
-            printArgsAsString = printArgsAsString .. printArg
-        end
-        result = result .. printArgsAsString .. "\n"
-    end
-    return result
-end
-
-local function PrintSavedOutput()
-    msg = ConvertSavedOutputToString()
-    print(msg)
-end
-
-local function IsContainedInSavedOutput(expected)
-    for _, printArgs in ipairs(savedOutput) do
-        local printArgsAsString = ""
-        for _, printArg in ipairs(printArgs) do
-            if expected == printArg then
-                return true
-            end
-        end
-    end
-    return false
-end
-
-local function AssertSavedOutputContains(expected)
-    local savedOutputAsString = ConvertSavedOutputToString()
-    local msg = string.format("expected captured output to contain: %s\nThe following output has been captured: %s",
-        expected, savedOutputAsString)
-
-    assert(IsContainedInSavedOutput(expected), msg)
-end
-
 function TestCommandLineRunner.setUp()
-    print = InterceptedPrint
+    pai.ReplacePrintByInterceptedPrint()
 end
 
 function TestCommandLineRunner.tearDown()
-    print = originalPrint
-    PrintSavedOutput()
+    pai.RestoreOriginalPrint()
 end
 
 function TestCommandLineRunner.test_advent_of_code_sample_data()
@@ -66,8 +17,7 @@ function TestCommandLineRunner.test_advent_of_code_sample_data()
     clr.Run(inputFile)
 
     local expected = 95437
-    AssertSavedOutputContains("" .. expected)
-    lu.assertTrue(IsContainedInSavedOutput("" .. expected))
+    pai.AssertSavedOutputContains("" .. expected)
 end
 
 return TestCommandLineRunner
---
+++ Day07NoSpaceLeftOnDevice/test/PrintArgumentInterceptor.lua
@@ -0,0 +1,76 @@
+-- This module redirects the Lua print command to an internal memory buffer
+--
+-- The module is intended for unit tests which want to assert that specific
+-- messages have been printed using the print command.
+--
+-- Usage:
+--
+-- 1. From the tearDown() method call RestoreOriginalPrint()
+-- 2. From the setUp() method of your unit test suite call ReplacePrintByInterceptedPrint()
+-- 3. Create the unit test executing code that uses the print command
+-- 4. Verify your expectations using AssertSavedOutputContains(expected)
+--
+-- Helpers for Debugging
+--
+-- - PrintSavedOutput() - print saved output bypassing the capture mechanism and using the original print command
+--
+local M = {}
+
+local savedOutput = {}
+local function SaveOutput(...)
+    table.insert(savedOutput, {...})
+end
+
+local originalPrint = print
+local function InterceptedPrint(...)
+    SaveOutput(...)
+    -- Uncomment the following line if you want to see the output for debugging
+    -- originalPrint(...)
+end
+
+function M.RestoreOriginalPrint()
+    print = originalPrint
+end
+
+function M.ReplacePrintByInterceptedPrint()
+    print = InterceptedPrint
+end
+
+local function ConvertSavedOutputToString()
+    local result = ""
+    for _, printArgs in ipairs(savedOutput) do
+        local printArgsAsString = ""
+        for _, printArg in ipairs(printArgs) do
+            printArgsAsString = printArgsAsString .. printArg
+        end
+        result = result .. printArgsAsString .. "\n"
+    end
+    return result
+end
+
+function M.PrintSavedOutput()
+    msg = ConvertSavedOutputToString()
+    originalPrint(msg)
+end
+
+local function IsContainedInSavedOutput(expected)
+    for _, printArgs in ipairs(savedOutput) do
+        local printArgsAsString = ""
+        for _, printArg in ipairs(printArgs) do
+            if expected == printArg then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+function M.AssertSavedOutputContains(expected)
+    local savedOutputAsString = ConvertSavedOutputToString()
+    local msg = string.format("expected captured output to contain: %s\nThe following output has been captured: %s",
+        expected, savedOutputAsString)
+
+    assert(IsContainedInSavedOutput(expected), msg)
+end
+
+return M
