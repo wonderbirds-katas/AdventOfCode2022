bcd02c264a3a2c80005b6638615ead3fde4fbca1
Stefan Boos <kontakt@boos.systems>
2023-01-12 20:28:48 +0100

    refactor: re-use test body to test different aspects of same setup

--- Day08TreetopTreeHouse/Day08TreetopTreeHouse.Tests/VisibleTreesCounterTests.cs
+++ Day08TreetopTreeHouse/Day08TreetopTreeHouse.Tests/VisibleTreesCounterTests.cs
@@ -11,43 +11,34 @@ public class VisibleTreesCounterTests
     
     [Fact]
     public void CountTreesVisibleFromLeft_SingleTree_Returns1()
-    {
-        var treeHeightGrid = new []{ "9" };
-        var actual = VisibleTreesCounter.CountTreesVisibleFromLeft(treeHeightGrid);
-        actual.Should().Be(1);
-    }
+        => GivenRowOfTrees_CountingVisibleFromLeft_ReturnsExpected("9", 1);
 
     [Theory]
     [InlineData("99")]
     [InlineData("11")]
     [InlineData("00")]
     public void CountTreesVisibleFromLeft_TwoTreesWithSameHeight_Returns1(string treeHeightRow)
-    {
-        var treeHeightGrid = new []{ treeHeightRow };
-        var actual = VisibleTreesCounter.CountTreesVisibleFromLeft(treeHeightGrid);
-        actual.Should().Be(1);
-    }
+        => GivenRowOfTrees_CountingVisibleFromLeft_ReturnsExpected(treeHeightRow, 1);
     
     [Theory]
     [InlineData("98")]
     [InlineData("10")]
     public void CountTreesVisibleFromLeft_TwoTreesWithDescendingHeight_Returns1(string treeHeightRow)
-    {
-        var treeHeightGrid = new []{ treeHeightRow };
-        var actual = VisibleTreesCounter.CountTreesVisibleFromLeft(treeHeightGrid);
-        actual.Should().Be(1);
-    }
+        => GivenRowOfTrees_CountingVisibleFromLeft_ReturnsExpected(treeHeightRow, 1);
 
     [Theory]
     [InlineData("89", 2)]
     [InlineData("789", 3)]
     [InlineData("6789", 4)]
     public void CountTreesVisibleFromLeft_TreesWithAscendingHeight_ReturnsCorrectNumber(string treeHeightRow, int expected)
+        => GivenRowOfTrees_CountingVisibleFromLeft_ReturnsExpected(treeHeightRow, expected);
+
+    private static void GivenRowOfTrees_CountingVisibleFromLeft_ReturnsExpected(string treeHeightRow, int expected)
     {
-        var treeHeightGrid = new []{ treeHeightRow };
+        var treeHeightGrid = new[] {treeHeightRow};
         var actual = VisibleTreesCounter.CountTreesVisibleFromLeft(treeHeightGrid);
         actual.Should().Be(expected);
     }
-    
+
     // TODO: Handle edge case of empty string in input tree patch string list
 }
