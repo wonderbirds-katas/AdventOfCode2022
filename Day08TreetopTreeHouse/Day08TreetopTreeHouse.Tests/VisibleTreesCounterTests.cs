ee97b9a248bc2351f51ddbf2ea76605c4948a73b
Stefan Boos <kontakt@boos.systems>
2023-01-12 05:43:15 +0100

    feat(8-1): count trees visible from left for single tree

--- Day08TreetopTreeHouse/Day08TreetopTreeHouse.Tests/VisibleTreesCounterTests.cs
+++ Day08TreetopTreeHouse/Day08TreetopTreeHouse.Tests/VisibleTreesCounterTests.cs
@@ -8,4 +8,12 @@ public class VisibleTreesCounterTests
         var actual = VisibleTreesCounter.CountTreesVisibleFromLeft(Array.Empty<string>());
         actual.Should().Be(0);
     }
+    
+    [Fact]
+    public void CountTreesVisibleFromLeft_SingleTree_Returns1()
+    {
+        var treeHeightGrid = new []{ "9" };
+        var actual = VisibleTreesCounter.CountTreesVisibleFromLeft(treeHeightGrid);
+        actual.Should().Be(1);
+    }
 }
--- Day08TreetopTreeHouse/Day08TreetopTreeHouse/VisibleTreesCounter.cs
+++ Day08TreetopTreeHouse/Day08TreetopTreeHouse/VisibleTreesCounter.cs
@@ -2,8 +2,12 @@ namespace Day08TreetopTreeHouse;
 
 public static class VisibleTreesCounter
 {
-    public static int CountTreesVisibleFromLeft(IEnumerable<string> _)
+    public static int CountTreesVisibleFromLeft(IList<string> treeHeightGrid)
     {
+        if (treeHeightGrid.Any())
+        {
+            return 1;
+        }
         return 0;
     }
 }
