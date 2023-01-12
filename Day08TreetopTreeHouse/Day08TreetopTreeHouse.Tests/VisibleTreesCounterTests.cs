87eb2881eafa5e9243ad875bc21c3b75c4537f90
Stefan Boos <kontakt@boos.systems>
2023-01-12 20:56:19 +0100

    refactor: increase readability of test cases
    by introducing a test fixture using fluent style of test definition

--- Day08TreetopTreeHouse/Day08TreetopTreeHouse.Tests/VisibleTreesCounterTests.cs
+++ Day08TreetopTreeHouse/Day08TreetopTreeHouse.Tests/VisibleTreesCounterTests.cs
@@ -8,37 +8,61 @@ public class VisibleTreesCounterTests
         var actual = VisibleTreesCounter.CountTreesVisibleFromLeft(Array.Empty<string>());
         actual.Should().Be(0);
     }
-    
+
     [Fact]
     public void CountTreesVisibleFromLeft_SingleTree_Returns1()
-        => GivenRowOfTrees_CountingVisibleFromLeft_ReturnsExpected("9", 1);
+        => GivenRowOfTreeHeights("9")
+            .WhenCountingVisibleFromLeft()
+            .ThenReturn(1);
 
     [Theory]
     [InlineData("99")]
     [InlineData("11")]
     [InlineData("00")]
     public void CountTreesVisibleFromLeft_TwoTreesWithSameHeight_Returns1(string treeHeightRow)
-        => GivenRowOfTrees_CountingVisibleFromLeft_ReturnsExpected(treeHeightRow, 1);
-    
+        => GivenRowOfTreeHeights(treeHeightRow)
+            .WhenCountingVisibleFromLeft()
+            .ThenReturn(1);
+
     [Theory]
     [InlineData("98")]
     [InlineData("10")]
     public void CountTreesVisibleFromLeft_TwoTreesWithDescendingHeight_Returns1(string treeHeightRow)
-        => GivenRowOfTrees_CountingVisibleFromLeft_ReturnsExpected(treeHeightRow, 1);
+        => GivenRowOfTreeHeights(treeHeightRow)
+            .WhenCountingVisibleFromLeft()
+            .ThenReturn(1);
 
     [Theory]
     [InlineData("89", 2)]
     [InlineData("789", 3)]
     [InlineData("6789", 4)]
     public void CountTreesVisibleFromLeft_TreesWithAscendingHeight_ReturnsCorrectNumber(string treeHeightRow, int expected)
-        => GivenRowOfTrees_CountingVisibleFromLeft_ReturnsExpected(treeHeightRow, expected);
-
-    private static void GivenRowOfTrees_CountingVisibleFromLeft_ReturnsExpected(string treeHeightRow, int expected)
-    {
-        var treeHeightGrid = new[] {treeHeightRow};
-        var actual = VisibleTreesCounter.CountTreesVisibleFromLeft(treeHeightGrid);
-        actual.Should().Be(expected);
-    }
+        => GivenRowOfTreeHeights(treeHeightRow)
+            .WhenCountingVisibleFromLeft()
+            .ThenReturn(expected);
 
     // TODO: Handle edge case of empty string in input tree patch string list
+    
+    private static TestFixture GivenRowOfTreeHeights(string treeHeightRow)
+    {
+        return new TestFixture(treeHeightRow);
+    }
+
+    internal class TestFixture
+    {
+        private readonly string _treeHeightRow;
+        private int _numberOfTreesVisibleFromLeft;
+
+        public TestFixture(string treeHeightRow)
+            => _treeHeightRow = treeHeightRow;
+
+        public TestFixture WhenCountingVisibleFromLeft()
+        {
+            _numberOfTreesVisibleFromLeft = VisibleTreesCounter.CountTreesVisibleFromLeft(new[] {_treeHeightRow});
+            return this;
+        }
+
+        public void ThenReturn(int expected)
+            => _numberOfTreesVisibleFromLeft.Should().Be(expected);
+    }
 }
