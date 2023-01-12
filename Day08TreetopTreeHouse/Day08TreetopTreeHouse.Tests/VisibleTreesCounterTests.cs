30d26b2a123d84b2a6108717905c784d3926640f
Stefan Boos <kontakt@boos.systems>
2023-01-12 05:46:41 +0100

    feat(8-1): count trees visible from left for two trees of same height

--- Day08TreetopTreeHouse/Day08TreetopTreeHouse.Tests/VisibleTreesCounterTests.cs
+++ Day08TreetopTreeHouse/Day08TreetopTreeHouse.Tests/VisibleTreesCounterTests.cs
@@ -16,4 +16,15 @@ public class VisibleTreesCounterTests
         var actual = VisibleTreesCounter.CountTreesVisibleFromLeft(treeHeightGrid);
         actual.Should().Be(1);
     }
+
+    [Theory]
+    [InlineData("99")]
+    [InlineData("11")]
+    [InlineData("00")]
+    public void CountTreesVisibleFromLeft_TwoTreesWithSameHeight_Returns1(string treeHeightRow)
+    {
+        var treeHeightGrid = new []{ treeHeightRow };
+        var actual = VisibleTreesCounter.CountTreesVisibleFromLeft(treeHeightGrid);
+        actual.Should().Be(1);
+    }
 }
