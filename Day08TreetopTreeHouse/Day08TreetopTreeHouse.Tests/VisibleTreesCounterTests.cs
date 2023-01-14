7146ce2498da0172d9d31446f2434d7bc37b4a8a
Stefan Boos <kontakt@boos.systems>
2023-01-14 15:33:19 +0100

    test: add a row to the multirow test

--- Day08TreetopTreeHouse/Day08TreetopTreeHouse.Tests/VisibleTreesCounterTests.cs
+++ Day08TreetopTreeHouse/Day08TreetopTreeHouse.Tests/VisibleTreesCounterTests.cs
@@ -14,11 +14,12 @@ public class VisibleTreesCounterTests
     {
         var treeHeightGrid = new[]
         {
-            "175389", // 1, 7, 8, 9 are visible from left + 9 is visible from the right
-            "042276" // 0, 4, 7 are visible from left + 6, 7 is visible from the right
+            "175389", // 1, 7, 8, 9 are visible from left + 9    is  visible from the right
+            "042276", // 0, 4, 7    are visible from left + 6, 7 are visible from the right
+            "367475", // 3, 6, 7    are visible from left + 5, 7 are visible from the right
         };
         
         var actual = VisibleTreesCounter.Count(treeHeightGrid);
-        actual.Should().Be(4 + 1 + 3 + 2);
+        actual.Should().Be(4 + 1 + 3 + 2 + 3 + 2);
     }
 }
