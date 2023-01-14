d6b8cfea3577dbeb0aeb9b4e6252a379fce05598
Stefan Boos <kontakt@boos.systems>
2023-01-14 21:40:31 +0100

    test: integration test shows error calculating the final result

--- Day08TreetopTreeHouse/Day08TreetopTreeHouse.Tests/ProgramTests.cs
+++ Day08TreetopTreeHouse/Day08TreetopTreeHouse.Tests/ProgramTests.cs
@@ -3,14 +3,14 @@ namespace Day08TreetopTreeHouse.Tests;
 public class ProgramTests
 {
     [Theory]
-    [InlineData("./data/SampleData.txt")]
-    public void ProcessInputFile(string testFileName)
+    [InlineData("./data/advent_of_code_example.txt", 21)]
+    public void ProcessInputFile(string testFileName, int expected)
     {
         var stringWriter = new StringWriter();
         Console.SetOut(stringWriter);
 
         Program.Main(new[] { testFileName });
 
-        stringWriter.ToString().Should().Be($"Input file is '{testFileName}'\n");
+        stringWriter.ToString().Should().Be($"{expected}\n");
     }
 }
--- Day08TreetopTreeHouse/Day08TreetopTreeHouse/Program.cs
+++ Day08TreetopTreeHouse/Day08TreetopTreeHouse/Program.cs
@@ -4,6 +4,7 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine($"Input file is '{args[0]}'");
+        var numberOfVisibleTrees = VisibleTreesCounter.Count(File.ReadAllLines(args[0]));
+        Console.WriteLine(numberOfVisibleTrees);
     }
 }
