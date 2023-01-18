6cc2742e00f14de89da1082ad4adda48a840b0df
Stefan Boos <kontakt@boos.systems>
2023-01-18 17:45:14 +0100

    chatgpt: Please create a factory method to construct a matrix from a list of strings with the same length
    Here is an example of a factory method that you can use to construct a matrix of a specified data type from a list of strings with the same length:
    
    You can use this factory method to construct a matrix from a list of strings by passing in a parse function that converts a string to the desired data type.
    For example, if you want to create a matrix of integers, you would call the factory method like this:
    
    ```csharp
    List<string> list = new List<string>() { "1234", "5678", "9012" };
    Matrix<int> matrix = Matrix<int>.FromList(list, int.Parse);
    ```
    
    or for string matrix
    
    ```csharp
    List<string> list = new List<string>() { "Hello", "World"};
    Matrix<string> matrix = Matrix<string>.FromList(list, x=> x);
    ```
    
    Please note that this is just one example of how you can implement a factory method to construct a matrix from a list of strings. Depending on your specific use case, you may need to make adjustments to this code to handle different types of input or edge cases.

--- Day08TreetopTreeHouse/Day08TreetopTreeHouse/Matrix.cs
+++ Day08TreetopTreeHouse/Day08TreetopTreeHouse/Matrix.cs
@@ -28,4 +28,29 @@ class Matrix<T>
     {
         get { return _matrix.GetLength(1); }
     }
+
+    public static Matrix<T> FromList(List<string> list, Func<string, T> parse)
+    {
+        if (list.Count == 0)
+        {
+            throw new ArgumentException("List cannot be empty");
+        }
+
+        int length = list[0].Length;
+        if (list.Any(x => x.Length != length))
+        {
+            throw new ArgumentException("All strings in the list must have the same length");
+        }
+
+        Matrix<T> matrix = new Matrix<T>(list.Count, length);
+        for (int row = 0; row < list.Count; row++)
+        {
+            for (int col = 0; col < length; col++)
+            {
+                matrix.SetValue(row, col, parse(list[row][col].ToString()));
+            }
+        }
+
+        return matrix;
+    }
 }
